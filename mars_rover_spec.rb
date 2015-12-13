f4cd7364c74097bf20a47ee35081ef8f2390838e
ormaechea <castilloandres17@gmail.com>
2015-12-13 23:12:02 +0100

    Add #position tests

---
+++ mars_rover_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'plateau'
+require_relative 'rover'
+
+describe Rover do
+  let(:new_rover){Rover.new([1,2,'N'],['M','R','R','M','L','R','L'], 'new_plateau')}
+
+  describe '#position' do
+    it 'checks the class type of #position' do
+      expect(new_rover.position).to be_kind_of(Array)
+    end
+    it 'checks length of #position Array' do
+      expect(new_rover.position.length).to eq(2)
+    end
+  end
+
+end
