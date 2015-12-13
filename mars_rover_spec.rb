86bbfcc720023de76b1b57c3f8583ac7871713c6
ormaechea <castilloandres17@gmail.com>
2015-12-13 23:20:44 +0100

    Add  #instructions tests

--- mars_rover_spec.rb
+++ mars_rover_spec.rb
@@ -4,20 +4,31 @@ require_relative 'rover'
 describe Rover do
   let(:new_rover){Rover.new([1,2,'N'],['M','R','R','M','L','R','L'], 'new_plateau')}
 
-  describe '#position' do
-    it 'checks the class type of #position' do
-      expect(new_rover.position).to be_kind_of(Array)
-    end
-    it 'checks length of #position Array' do
-      expect(new_rover.position.length).to eq(2)
-    end
-  end
+    describe '#position' do
+      it 'checks the class type of #position' do
+        expect(new_rover.position).to be_kind_of(Array)
+      end
 
-   describe '#rover_orientation' do
-    it 'checks the data type of #orientation' do
-      expect(new_rover.orientation).to be_kind_of(String)
-    end
-  end
+      it 'checks length of #position Array' do
+        expect(new_rover.position.length).to eq(2)
+      end
 
+    end
+
+    describe '#rover_orientation' do
+      it 'checks the data type of #orientation' do
+        expect(new_rover.orientation).to be_kind_of(String)
+      end
+    end
+
+    describe '#instructions' do
+      it 'checks the class type of #instructions' do
+        expect(new_rover.instructions).to be_kind_of(Array)
+      end
+
+      it 'checks the data type of #instructions Array' do
+        expect(new_rover.instructions).to all(be_a(String))
+      end
+    end
 
 end
